2e70aaa9d20be338bb6e71516764e776afb22870
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2026-04-27 15:28:56 +0600

    add : data

--- task 2 - CNN implementation/src/config.py
+++ task 2 - CNN implementation/src/config.py
@@ -2,4 +2,4 @@ DATASET = "andrewmvd/lung-and-colon-cancer-histopathological-images"
 DATA_DIR = "data"
 IMG_SIZE = 224
 BATCH_SIZE = 32
-NUM_CLASSES = 3   # lung classes only
+NUM_CLASSES = 3   # lung classes
