594c274d1040d50834eb3b8fbf6b4f63442c19a3
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2026-04-29 09:39:28 +0600

    delete : task 2

--- task 2 - CNN implementation/src/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def download_dataset():
-    if not os.path.exists("data"):
-        print("Downloading dataset...")
-        os.system("kaggle datasets download -d andrewmvd/lung-and-colon-cancer-histopathological-images")
-        os.system("unzip lung-and-colon-cancer-histopathological-images.zip -d data")
-    else:
-        print("Dataset already exists.")
