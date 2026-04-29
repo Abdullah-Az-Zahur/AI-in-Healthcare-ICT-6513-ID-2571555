4e8789a62ae33d37800583c1c9baf1931ea9ce9f
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2026-04-29 15:39:15 +0600

    add : files

--- task 2 - CNN implementation/utils/metrics.py
+++
@@ -1,9 +0,0 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
-def calculate_metrics(y_true, y_pred):
-    acc = accuracy_score(y_true, y_pred)
-    prec = precision_score(y_true, y_pred)
-    rec = recall_score(y_true, y_pred)
-    f1 = f1_score(y_true, y_pred)
-
-    return acc, prec, rec, f1
